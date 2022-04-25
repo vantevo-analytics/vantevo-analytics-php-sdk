a4c8acc165324c7282d126e5c40627625d814d57
Andone Alessandro <andonealessandro@outlook.it>
2022-04-25 14:26:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,122 @@
+# The analysis of your website, but simpler
+ 
+**Vantevo Analytics** is the alternative platform to Google Analytics that respects privacy, because it does not need cookies not compliant with the GDPR. Easy to use, light and can be integrated into any website and back-end.
+ 
+This is the official PHP client SDK for Vantevo Analytics. For more information visit the website  [vantevo.io](https://vantevo.io).
+
+## Installation
+ 
+You can install the SDK using Composer or by copying the `src/Client.php` directly.
+
+`composer require vantevo-analytics/sdk`
+
+## Usage 
+
+To start tracking page views, events, and getting statistics, you need to initialize the client first:
+
+| Option        | Type                  | Description                 |
+| ------------- | --------------------- | ----------------------------|
+| accessToken   | `string`  (required)  | To create an api key read our [guide](https://vantevo.io/docs/account/impostazioni).|
+| domain        | `string`  (required)  | Enter the domain you want to use to collect statistics.The domain must not include http, https, or www. Example: example.com  |
+| timeout       | `int` (optional)      |  You can change the request timeout, you need to enter a number in seconds. Default `30`.|
+| dev           | `boolean` (optional)  | Tracker will not send data to server, the client execute print the data. Default `false`. |
+
+
+```php
+require __DIR__ . '/vendor/autoload.php'; // or require_once 'src/Client.php';
+
+$client = new Vantevo\Client('accessToken', 'domain', 'timeout' 'dev');
+```
+
+## Tracking page views and events
+
+**Parameters**
+
+| Option        | Type                  | Description                 |
+| ------------- | --------------------- | ----------------------------|
+| event   | `array`  (required)        | See event parameters. |
+
+**parameters**
+| Option        | Type                  | Description                 |
+| ------------- | --------------------- | ----------------------------|
+| event         | `string`  (required)  | Event name, remember that the name `pageview` will send a pageview event. |
+| url           | `string`  (optional)    | Enter url you want to save in the statistics. Default is where the client is installed. |
+| title         | `string`  (optional)    | You can insert a title of the page, if this field is not used vantevo will insert the pathname of the url used. |
+| referrer      | `string`  (optional)    | In this field you can enter a referrer for your request.  Default `$_SERVER[HTTP_REFERER]` or the client checks query parameters: `ref, referrer, source, utm_source`. |
+| width         | `string`  (optional)    | This field is used to save the screen size. Default: `0`.|
+| height        | `string`  (optional)    | This field is used to save the screen size.  Default: `0`. |
+| meta          | `array`  (optional)    | Enter the event values `meta_key` and` meta_value`, [read more how to create an event](https://vantevo.io/docs/come-creare-un-evento#evento)  Default: `array`. |
+
+### Example pageview
+
+```php
+try {
+    $data = array("event" => "pageview", "title" => "Eaxmple Page view");
+	$client->event($data);
+} catch (Exception $e) {
+	// something went wrong...
+}
+```
+
+### Example event
+
+```php
+try {
+    $data = array("event" => "Download", "meta" => array("pdf": "Recipes"));
+	$client->event($data);
+} catch (Exception $e) {
+	// something went wrong...
+}
+```
+
+
+## How to get the statistics
+
+**Parameters**
+
+| Option        | Type                  | Description                 |
+| ------------- | --------------------- | ----------------------------|
+| event         | `array`  (required)  | Check out our guide to see all the parameters you can use like, [click here](https://vantevo.io/docs/api-sdk/api-statistiche#parametri). |
+
+### Example Statistics
+
+```php
+try {
+    $data = array("source" => "pages", "period" => "1m"));
+	$stats = $client->stats($data);
+} catch (Exception $e) {
+	// something went wrong...
+}
+```
+
+### Example Events
+
+```php
+try {
+    $data = array("source" => "events", "period" => "1m");
+	$events = $client->events($data);
+} catch (Exception $e) {
+	// something went wrong...
+}
+```
+
+## Vantevo Analytics guide
+ 
+To see all the features and settings of Vantevo Analytics we recommend that you read our complete guide [here](https://vantevo.io/docs?utm_source=npm&utm_medium=vantevo-analytics-tracker).
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Client.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace Vantevo;
+
+class Client
+{
+    const DEFAULT_BASE_URL = "https://api.vantevo.io";
+    const SEND_EVENT_API = "http://localhost:5021/v1/event";
+    const STATS_API = "https://api.vantevo.io/v1/";
+
+
+    const REFERRER_PARAMS = array(
+        'ref',
+        'referer',
+        'referrer',
+        'source',
+        'utm_source'
+    );
+
+    private $accessToken;
+    private $domain;
+    private $timeout = 30;
+    private $dev = false;
+
+
+    function __construct($accessToken, $domain, $timeout, $dev = false)
+    {
+        if ($dev) {
+            $this->dev = $dev;
+        }
+        if ($timeout) {
+            $this->timeout = $timeout;
+        }
+        $this->domain = $domain;
+        $this->accessToken = $accessToken;
+    }
+
+    function event($event, $retry = true)
+    {
+
+        $default_hit = array(
+            "title" => null,
+            "url" => $this->getRequestURL(),
+            "referrer" => $this->getReferrer(),
+            "width" => null,
+            "height" => null,
+            "meta" => json_encode([]),
+        );
+
+        $hit = array_merge($default_hit, $event);
+
+        if ($this->dev) {
+            return print_r($hit);
+        }
+
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => array(
+                    'Content-Type: application/json',
+                    'User-Agent: ' . $this->getHeader("HTTP_USER_AGENT")
+                ),
+                'content' => json_encode($hit),
+                'timeout' => $this->timeout
+            )
+        );
+
+        $context = stream_context_create($options);
+        $result = @file_get_contents(self::SEND_EVENT_API, false, $context);
+
+
+        if ($result === FALSE) {
+            if ($retry) {
+                return  $this->event($event, false);
+            } else {
+                $responseHeader = $http_response_header[0];
+                throw new \Exception('Error request: ' . $responseHeader);
+            }
+        }
+
+        return $result;
+    }
+
+    function stats($filters)
+    {
+        return $this->sendRequest("stats", $filters, true);
+    }
+
+    function events($filters)
+    {
+        return $this->sendRequest("events", $filters, true);
+    }
+
+    private function sendRequest($type, $filters, $retry = true)
+    {
+
+        $options = array(
+            'http' => array(
+                'method' => 'GET',
+                'header' => 'Authorization: Bearer ' . $this->accessToken,
+                'timeout' => $this->timeout
+            )
+        );
+        $context = stream_context_create($options);
+        $filters["domain"] = $this->domain;
+        $result = @file_get_contents(self::STATS_API . $type . "?" . http_build_query($filters), false, $context);
+        if ($result === FALSE) {
+            if ($retry) {
+                return  $this->sendRequest($type, $filters, false);
+            } else {
+                $responseHeader = $http_response_header[0];
+                //$error = error_get_last();
+                //echo "HTTP request failed. Error was: " . $error['message'];
+                throw new \Exception('Error request: ' . $responseHeader);
+            }
+        }
+
+        if (is_object(json_decode($result))) {
+            return json_decode($result);
+        }
+
+        if ($retry) {
+            return $this->sendRequest($type, $filters, false);
+        } else {
+            throw new \Exception('Error request: ' . $result);
+        }
+    }
+
+    private function getRequestURL()
+    {
+        return 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    }
+
+    private function getReferrer()
+    {
+        $referrer = $this->getInfoHeader('HTTP_REFERER');
+        if (empty($referrer)) {
+            foreach (self::REFERRER_PARAMS as $key) {
+                $referrer = $this->getQueryParams($key);
+
+                if ($referrer != '') {
+                    return $referrer;
+                }
+            }
+        }
+
+        return $referrer;
+    }
+
+    private function getHeader($name)
+    {
+        if (isset($_SERVER[$name])) {
+            return $_SERVER[$name];
+        }
+
+        return '';
+    }
+
+    private function getQueryParams($name)
+    {
+        if (isset($_GET[$name])) {
+            return $_GET[$name];
+        }
+
+        return '';
+    }
+
+    private function getInfoHeader($name)
+    {
+        if (isset($_SERVER[$name])) {
+            return $_SERVER[$name];
+        }
+
+        return '';
+    }
+}
